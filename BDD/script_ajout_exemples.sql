7bb1171eb18eea380ddda6de8ff72e22f3d7ea9d
Jacques Le Guédès <jacques.le-guedes@isen-ouest.yncrea.fr>
2023-02-07 17:15:11 +0100

    Values add

--- BDD/script_ajout_exemples.sql
+++ BDD/script_ajout_exemples.sql
@@ -1,17 +1,17 @@
 INSERT INTO docteurs VALUES ("david.legall@gmai.com","LE GALL", "David", 0652123259, "1959-11-28","CENTRE HOSPITALIER FOUGERES");
-INSERT INTO est_specialiste_de VALUES ("david.legall@gmail.com", "Radiologue");
+INSERT INTO est_specialiste_de VALUES ("Radiologue","david.legall@gmail.com");
 
 INSERT INTO docteurs VALUES ("christelle.gwenn@gmai.com","GWENN", "Christelle", 0745123796, "1984-02-15","CHRU BRETONNEAU TOURS");
-INSERT INTO est_specialiste_de VALUES ("christelle.gwenn@gmail.com", "Psychiatre");
+INSERT INTO est_specialiste_de VALUES ("Psychiatre","christelle.gwenn@gmail.com");
 
 INSERT INTO docteurs VALUES ("laurent.dubois@gmai.com","DUBOIS", "Laurent", 0685423691, "1968-09-22","SITE DE PERTUIS");
-INSERT INTO est_specialiste_de VALUES ("laurent.dubois@gmail.com", "Chirurgien");
+INSERT INTO est_specialiste_de VALUES ("Chirurgien","laurent.dubois@gmail.com");
 
 INSERT INTO docteurs VALUES ("john.morston@gmai.com","MORSTON", "John", 0754665213, "1975-01-12","CLINIQUE DE LA MISERICORDE CAEN");
-INSERT INTO est_specialiste_de VALUES ("john.morston@gmai.com", "Cardiologue");
+INSERT INTO est_specialiste_de VALUES ("Cardiologue", "john.morston@gmai.com");
 
 INSERT INTO docteurs VALUES ("arthur.morgann@gmai.com","MORGANN", "Arthur", 0644551321, "1972-08-18","POLYCLINIQUE DU TREGOR");
-INSERT INTO est_specialiste_de VALUES ("arthur.morgann@gmai.com", "Allergologue");
+INSERT INTO est_specialiste_de VALUES ("Allergologue","arthur.morgann@gmai.com");
 
 INSERT INTO docteurs VALUES ("leon.kennedy@gmail.com","KENNEDY", "LEON", 0679563214, "1980-06-04","CLINIQUE DES GRAINETIERES");
-INSERT INTO est_specialiste_de VALUES ("leon.kennedy@gmail.com", "Orthoptiste");
+INSERT INTO est_specialiste_de VALUES ("Orthoptiste","leon.kennedy@gmail.com");
