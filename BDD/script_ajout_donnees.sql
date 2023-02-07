c24800fe3c64412f2c169ab21297eaa5f81e9ca0
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-02-07 08:40:34 +0100

    recupération professions méciales

---
+++ BDD/script_ajout_donnees.sql
@@ -0,0 +1,71 @@
+Acupuncteur / Acupunctrice
+Agent thermal / Agente thermale
+Aide-soignante / Aide-soignant
+Allergologue
+Ambulancier / Ambulancière
+Anesthésiste-réanimateur / Anesthésiste réanimatrice
+Animalier / Animalière de laboratoire
+Assistant / Assistante de régulation médicale - ARM
+Assistant médical / Assistante médicale
+Assistante dentaire / Assistant dentaire
+Attaché / Attachée d’administration hospitalière — Audioprothésiste
+Auxiliaire de puériculture - AP
+Cadre de santé
+Cardiologue
+Chiropracteur / Chiropractice
+Chirurgien / Chirurgienne
+Chirurgien-dentiste / Chirurgienne-dentiste
+Délégué / Déléguée de l’assurance maladie
+Délégué / Déléguée pharmaceutique
+Dermatologue
+Diététicien / Diététicienne
+Directeur / Directrice des soins — DS
+Directeur / Directrice d'établissement sanitaire, social et médico-social — DESSMS
+Directeur / Directrice d’hôpital — DH
+Equithérapeute
+Ergothérapeute
+Pagination
+Gériatre
+Gynécologue - obstétricien / Gynécologue - obstétricienne
+Infirmier / Infirmière anesthésiste
+Infirmier / Infirmière de bloc opératoire
+Infirmier / Infirmière en pratique avancée
+Infirmier / Infirmière hygiéniste
+Infirmier / Infirmière militaire
+Infirmière / Infirmier
+Ingénieur / Ingénieure de recherche produit
+Ingénieur / Ingénieure en recherche clinique
+Ingénieur / Ingénieure sécurité sanitaire
+Ingénieur hospitalier / Ingénieure hospitalière
+Inspecteur / Inspectrice de l’action sanitaire et sociale — IASS
+Kinésithérapeute
+Manipulatrice / Manipulateur d'électroradiologie médicale
+Médecin
+Médecin légiste
+Médecin militaire
+Ophtalmologiste
+Opticien - lunetier / Opticienne - lunetière
+Optométriste
+Orthopédiste-orthésiste
+Orthophoniste
+Orthoprothésiste
+Orthoptiste
+Ostéopathe
+Pédiatre
+Pédicure - podologue
+Pédopsychiatre
+Pharmacien / Pharmacienne
+Physicien médical / Physicienne médicale
+Phytothérapeute / Conseiller en phytothérapie
+Podo-orthésiste
+Préparateur / Préparatrice en pharmacie
+Prothésiste dentaire
+Psychiatre
+Psychomotricien / Psychomotricienne
+Puéricultrice/ Puériculteur
+Radiologue
+Sage-femme
+Secrétaire médical / Secrétaire médicale
+Technicien / Technicienne biologiste
+Technicien / Technicienne de laboratoire médical
+Visiteur médical / Visiteuse médicale
