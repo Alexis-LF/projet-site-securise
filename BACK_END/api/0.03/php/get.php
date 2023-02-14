dbcbb5e1fa18217059e79269f199ef328ec58801
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-02-14 18:11:23 +0100

    requête de la liste de docteurs avec leur profession, le site & la ville

--- BACK_END/api/0.03/php/get.php
+++ BACK_END/api/0.03/php/get.php
@@ -28,7 +28,15 @@ function professions()
 
 function recherche()
 {
-    return "SELECT * FROM docteurs;";
+    $requete = "SELECT d.nom, d.prenom, p.nom AS 'profession', d.telephone, d.mail, s.nom AS 'site', s.adresse, c.zip_code, c.name AS 'ville' ";
+    $requete .= "FROM est_specialiste_de e ";
+    $requete .= "JOIN docteurs d ON d.mail = e.mail ";
+    $requete .= "JOIN profession p ON p.nom = e.nom ";
+    $requete .= "JOIN site s ON s.nom = d.nom_site ";
+    $requete .= "JOIN cities c ON c.id = s.id; ";
+
+    return $requete;
+
 }
 
 
