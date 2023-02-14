3cda628b0428ae35585b3dfe2e0cc77fe3a1d3dc
Jacques Le Guédès <jacques.le-guedes@isen-ouest.yncrea.fr>
2023-02-14 17:57:31 +0100

    afficheDocteurs

--- FRONT_END/JavaScript/template.js
+++ FRONT_END/JavaScript/template.js
@@ -35,27 +35,7 @@ function w3_open() {
     x[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " w3-opacity-off";
   }
-
-
-  // function afficheDocteurs(liste_docteurs){
-  //   console.log(liste_docteurs);
-
-  
-  //   let html_mere=document.getElementById("liste_medecins"); //parent
-  //   let docteur=liste_docteurs[0];
-    
-  //   let div = document.createElement("div");
-  //   let nom = document.createElement("p"); // enfant
-  //   nom.appendChild(document.createTextNode('Nom : '+ docteur["nom"]));
-  //   div.appendChild(nom);
-  //   html_mere.appendChild(nom);
-
-
-  // }
-  // ajaxRequest('GET', 'http://api.projetm1.fr/0.01/', afficheDocteurs);
-
-
-  
+  // Fonction affiche docteurs 
     function afficheDocteurs(liste_docteurs){
       for (let i = 0; i <= liste_docteurs.length; i++) {
       console.log(liste_docteurs);
@@ -111,65 +91,7 @@ function w3_open() {
     };
     }
     
+
   ajaxRequest('GET', 'http://api.projetm1.fr/0.01/', afficheDocteurs);
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   /* const label1 = document.createElement('label');
-    const input1 = document.createElement('input');
-    label1.appendChild(document.createTextNode('Nom : '));
-    
-    form.appendChild(label1);
   
-    const label2 = document.createElement('label');
-    const input2 = document.createElement('input');
-    input2.type = 'text';
-    label2.appendChild(document.createTextNode('Prénom : '));
-    label2.appendChild(input2);
-    form.appendChild(label2);
-  
-    const label3 = document.createElement('label');
-    const input3 = document.createElement('input');
-    input3.type = 'email';
-    label3.appendChild(document.createTextNode('Mail : '));
-    label3.appendChild(input3);
-    form.appendChild(label3);
-  
-    const label4 = document.createElement('label');
-    const input4 = document.createElement('input');
-    input4.type = 'tel';
-    label4.appendChild(document.createTextNode('Numéro portable : '));
-    label4.appendChild(input4);
-    form.appendChild(label4);
-  
-    const label5 = document.createElement('label');
-    const input5 = document.createElement('input');
-    input5.type = 'date';
-    label5.appendChild(document.createTextNode('Date de naissance : '));
-    label5.appendChild(input5);
-    form.appendChild(label5);
-  
-    const label6 = document.createElement('label');
-    const input6 = document.createElement('input');
-    input6.type = 'text';
-    label6.appendChild(document.createTextNode('Nom du site : '));
-    label6.appendChild(input6);
-    form.appendChild(label6);
-  
-    document.body.appendChild(form);
-  }
-  */
