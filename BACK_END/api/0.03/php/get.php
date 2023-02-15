224b6931375111314143d698f5842f2df268976e
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-02-15 10:24:17 +0100

    recherche selon critètes en cours

--- BACK_END/api/0.03/php/get.php
+++ BACK_END/api/0.03/php/get.php
@@ -10,7 +10,7 @@ function requetesGet(){
         break;
     case "/recherche":
         return recherche();
-        break;        
+        break;
     default:
        return "false";
     }
@@ -28,12 +28,31 @@ function professions()
 
 function recherche()
 {
+
     $requete = "SELECT d.nom, d.prenom, p.nom AS 'profession', d.telephone, d.mail, s.nom AS 'site', s.adresse, c.zip_code, c.name AS 'ville' ";
     $requete .= "FROM est_specialiste_de e ";
     $requete .= "JOIN docteurs d ON d.mail = e.mail ";
     $requete .= "JOIN profession p ON p.nom = e.nom ";
     $requete .= "JOIN site s ON s.nom = d.nom_site ";
-    $requete .= "JOIN cities c ON c.id = s.id; ";
+    $requete .= "JOIN cities c ON c.id = s.id ";
+
+    // s'il n'y a aucun paramètre de recherche on exécute la requête sans conditions de recherche
+    if (count($_GET)==0){
+        $requete .= ";";
+        return $requete;
+    }
+
+    // // ajout des conditions de recherche
+    // $recherche = "WHERE ";
+
+    // foreach ($_GET[""] as $clef => $valeur) {
+    //     // on ajoute "AND" si ce n'est pas la 1re condition citée
+    //     if($recherche != "WHERE "){
+    //         $recherche .= "AND ";
+    //     }
+    //     $recherche .= $clef." LIKE %".$valeur."% ";
+
+    // }
 
     return $requete;
 
