b63e7e56162c0bd94b5fc236ab2edf9d1bef6844
Jacques Le Guédès <jacques.le-guedes@isen-ouest.yncrea.fr>
2023-02-16 17:39:51 +0100

    Verification mot de passe

--- FRONT_END/javascript/gestionMotDePasse.js
+++ FRONT_END/javascript/gestionMotDePasse.js
@@ -29,3 +29,33 @@ function verifierMotDePasse() {
     // Si toutes les conditions sont remplies, renvoyer true
     return true;
 }
+
+function validerFormulaire() {
+    const nom = document.getElementsByName("nom")[0].value;
+    const prenom = document.getElementsByName("prenom")[0].value;
+    const dateNaissance = document.getElementsByName("dateNaissance")[0].value;
+    const email = document.getElementsByName("email")[0].value;
+    const motDePasse = document.getElementsByName("motDePasse")[0].value;
+    const confirmationMotDePasse = document.getElementsByName("confirmationMotDePasse")[0].value;
+    const numeroPortable = document.getElementsByName("numeroPortable")[0].value;
+
+    // Vérifier que tous les champs sont remplis
+    if (!nom || !prenom || !dateNaissance || !email || !motDePasse || !confirmationMotDePasse || !numeroPortable) {
+      alert("Veuillez remplir tous les champs du formulaire.");
+      console.log("Veuillez remplir tous les champs du formulaire.")
+      return;
+    }
+
+    // Vérifier que le mot de passe répond aux exigences spécifiées
+    const regexMotDePasse = /(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[A-Z])/;
+    if (!regexMotDePasse.test(motDePasse)) {
+      alert("Le mot de passe doit contenir au moins un caractère spécial et une majuscule.");
+      console.log("Le mot de passe doit contenir au moins un caractère spécial et une majuscule.")
+      return;
+    }
+
+    // Rediriger l'utilisateur vers la page index.html
+    // if (!nom || !prenom || !dateNaissance || !email || !motDePasse || !confirmationMotDePasse || !numeroPortable || !regexMotDePasse.test(motDePasse) ){
+    window.location.href = "../index.html";
+    // }
+  }
