d95142ecda7b7ebb2c7f1d5af04a6ae7b22344f1
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-02-17 12:46:37 +0100

    pour tester on passe par du GET plutôt que du post

--- BACK_END/api/0.04/index.php
+++ BACK_END/api/0.04/index.php
@@ -4,15 +4,13 @@ include_once("php/get.php");
 include_once("php/post.php");
 include_once("php/fonctions.php");
 
-// contiendra la phrase à taper sur mysql
-$phraseRequete = "";
-
 // On vérifie si c'est un GET, POST, PUT, et on redirige le traitement aux fonctions appropriées
 switch ($_SERVER['REQUEST_METHOD']) {
+    // case "GET":
+    //     reponseJSONrequeteSQL(requetesGet());
+    //     break;
+    // case "POST":
     case "GET":
-        reponseJSONrequeteSQL(requetesGet());
-        break;
-    case "POST":
         requetesPost();
         break;
     default:
@@ -20,7 +18,6 @@ switch ($_SERVER['REQUEST_METHOD']) {
         echo "Bad Request";
         exit;
 }
-
 
 
 ?>
