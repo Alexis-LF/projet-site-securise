fb214008a490bf5bc88f86e14bc1f6c90c1101f9
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-02-17 12:23:01 +0100

    identification et connexion qui retourne un JWT

--- BACK_END/api/0.04/php/post.php
+++ BACK_END/api/0.04/php/post.php
@@ -1,8 +1,9 @@
 <?php
 include_once("fonctions_jwt.php");
+include_once("pdo.php");
 
 // Aiguillage des requêtes de type POST
-function requetesGet(){
+function requetesPost(){
     switch ($_SERVER["PATH_INFO"]) {
     case "/connexion":
         return connexion();
@@ -12,9 +13,39 @@ function requetesGet(){
     }
 }
 
+// vérifier l'identification de la personne
+function identification(){
+    // on fait exprès de ne pas sécuriser pour se faire passer pour quelqu'un d'autre assez facilement
+    $bdd = connexionBDD();
+    $phraseRequete = "SELECT mail FROM patients WHERE mail=".$_POST["mail"].";";
+    $reponse = requeteBDD($bdd , $phraseRequete);
+    if ($reponse){
+        return (string) $reponse[0]["mail"];
+    }
+    return "false";
+
+}
+
+
 function connexion()
 {
-    $jwt = encodeJWT($_POST["mail"]);
+    $mail = identification();
+    // Connexion réussie
+    if ($mail!="false"){
+        $jwt = encodeJWT($_POST["mail"]);
+        header('Content-Type: text/plain; charset=utf-8');
+        header('Cache-control: no-store, no-cache, must-revalidate');
+        header('Pragma: no-cache');
+        header('Access-Control-Allow-Origin: *');
+        // code de réponse HTTP
+        header('HTTP/1.1 201 Created');
+        echo $jwt;
+        exit;
+    }
+    // Échec de la connexion.
+    header('HTTP/1.1 401 Unauthorized');
+    echo "Connexion failed, please check your id and pwd";
+    exit;
 }
 
 ?>
