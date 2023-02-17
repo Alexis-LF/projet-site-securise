30168692340af5fccf24bfc3c1087e150261ff90
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-02-17 11:57:22 +0100

    la transfrmation de la réponse de la BDD en JSON est dans pdo.php désormais

--- BACK_END/api/0.04/index.php
+++ BACK_END/api/0.04/index.php
@@ -3,9 +3,6 @@ include_once("php/pdo.php");
 include_once("php/get.php");
 include_once("php/fonctions.php");
 
-// la variable bdd est l'objet avec lequel intéragir pour intérroger la base de donnée
-$bdd = connexionBDD();
-
 // contiendra la phrase à taper sur mysql
 $phraseRequete = "";
 
@@ -18,44 +15,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
         $phraseRequete = "false";
 }
 
-// Message d'erreur si mauvaise requête écrite
-if($phraseRequete == "false"){
-    header('HTTP/1.1 400 Bad Request');
-    echo "Bad Request";
-    exit;
-}
+reponseJSONrequeteSQL($phraseRequete);
 
-// Décommenter les lignes ci-dessous pour afficher les requêtes SQL prêtes à être envoyées
-// echo $phraseRequete;
-// exit;
-
-// On rajoute une limite à la requête SQL pour ne pas faire planter le traitement des données du front-end
-$phraseRequete = str_replace(";"," LIMIT ".LIMITE_RESULTATS.";",$phraseRequete);
-// lancement d'une requête
-$reponse = requeteBDD($bdd , $phraseRequete);
-
-if ($reponse)
-{
-    // headers pour la mise à jour de la page et pour le JSON
-    header('Content-Type: application/json; charset=utf-8');
-    header('Cache-control: no-store, no-cache, must-revalidate');
-    header('Pragma: no-cache');
-    // Pour que le CORS du navigateur ne bloque pas le chargement
-    header('Access-Control-Allow-Origin: *');
-    // code de réponse HTTP
-    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        header('HTTP/1.1 201 Created');
-    }
-    else{
-        header('HTTP/1.1 200 OK');
-    }
-    // envoi des données
-    echo json_encode($reponse);
-    exit;
-}
-// Mauvaise requête envoyée.
-header('HTTP/1.1 400 Bad Request');
-echo "Bad Request";
-exit;
 ?>
 
