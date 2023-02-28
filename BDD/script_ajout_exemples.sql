dd0634e60db99a8b630ed0c5df65804cacc2e472
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-02-28 09:12:23 +0100

    ajout d'ex de facuture et optimisation des ajouts

--- BDD/script_ajout_exemples.sql
+++ BDD/script_ajout_exemples.sql
@@ -1,6 +1,7 @@
 #------------------------------------------------------------
 # DELETE des exemples
 #------------------------------------------------------------
+DELETE FROM facture;
 DELETE FROM documents;
 DELETE FROM est_specialiste_de;
 DELETE FROM docteurs;
@@ -11,27 +12,24 @@ DELETE FROM patients;
 #------------------------------------------------------------
 # Table : docteurs et est_specialiste_de
 #------------------------------------------------------------
-INSERT INTO docteurs VALUES ("david.legall@gmail.com","LE GALL", "David", 0652123259, "1959-11-28","CENTRE HOSPITALIER FOUGERES");
-INSERT INTO est_specialiste_de VALUES ("Radiologue","david.legall@gmail.com");
+INSERT INTO docteurs(mail,nom,prenom,telephone,date_naissance,nom_site) VALUES
+("david.legall@gmail.com","LE GALL", "David", 0652123259, "1959-11-28","CENTRE HOSPITALIER FOUGERES"),
+("christelle.gwenn@gmail.com","GWENN", "Christelle", 0745123796, "1984-02-15","CHRU BRETONNEAU TOURS"),
+("laurent.dubois@gmail.com","DUBOIS", "Laurent", 0685423691, "1968-09-22","SITE DE PERTUIS"),
+("john.morston@gmail.com","MORSTON", "John", 0754665213, "1975-01-12","CLINIQUE DE LA MISERICORDE CAEN"),
+("arthur.morgann@gmail.com","MORGANN", "Arthur", 0644551321, "1972-08-18","POLYCLINIQUE DU TREGOR"),
+("leon.kennedy@gmail.com","KENNEDY", "LEON", 0679563214, "1980-06-04","CLINIQUE DES GRAINETIERES"),
+("leona.kenneda@gmail.com","KENNEDA", "LEONA", 0679563214, "1980-06-04","POLYCLINIQUE DU TREGOR");
 
-INSERT INTO docteurs VALUES ("christelle.gwenn@gmail.com","GWENN", "Christelle", 0745123796, "1984-02-15","CHRU BRETONNEAU TOURS");
-INSERT INTO est_specialiste_de VALUES ("Psychiatre","christelle.gwenn@gmail.com");
-
-INSERT INTO docteurs VALUES ("laurent.dubois@gmail.com","DUBOIS", "Laurent", 0685423691, "1968-09-22","SITE DE PERTUIS");
-INSERT INTO est_specialiste_de VALUES ("Chirurgien / Chirurgienne","laurent.dubois@gmail.com");
-
-INSERT INTO docteurs VALUES ("john.morston@gmail.com","MORSTON", "John", 0754665213, "1975-01-12","CLINIQUE DE LA MISERICORDE CAEN");
-INSERT INTO est_specialiste_de VALUES ("Cardiologue", "john.morston@gmail.com");
-INSERT INTO est_specialiste_de VALUES ("Orthoptiste", "john.morston@gmail.com");
-
-INSERT INTO docteurs VALUES ("arthur.morgann@gmail.com","MORGANN", "Arthur", 0644551321, "1972-08-18","POLYCLINIQUE DU TREGOR");
-INSERT INTO est_specialiste_de VALUES ("Allergologue","arthur.morgann@gmail.com");
-
-INSERT INTO docteurs VALUES ("leon.kennedy@gmail.com","KENNEDY", "LEON", 0679563214, "1980-06-04","CLINIQUE DES GRAINETIERES");
-INSERT INTO est_specialiste_de VALUES ("Orthoptiste","leon.kennedy@gmail.com");
-
-INSERT INTO docteurs VALUES ("leona.kenneda@gmail.com","KENNEDA", "LEONA", 0679563214, "1980-06-04","POLYCLINIQUE DU TREGOR");
-INSERT INTO est_specialiste_de VALUES ("Orthoptiste","leona.kenneda@gmail.com");
+INSERT INTO est_specialiste_de(nom,mail) VALUES 
+("Radiologue","david.legall@gmail.com"),
+("Psychiatre","christelle.gwenn@gmail.com"),
+("Chirurgien / Chirurgienne","laurent.dubois@gmail.com"),
+("Cardiologue", "john.morston@gmail.com"),
+("Orthoptiste", "john.morston@gmail.com"),
+("Allergologue","arthur.morgann@gmail.com"),
+("Orthoptiste","leon.kennedy@gmail.com"),
+("Orthoptiste","leona.kenneda@gmail.com");
 
 #------------------------------------------------------------
 # Table : patients et personne
@@ -43,7 +41,28 @@ INSERT INTO personne VALUES ("mail@test.com","Nastique","Jim",0605040302, "2001-
 #------------------------------------------------------------
 # Table : documents
 #------------------------------------------------------------
-INSERT INTO documents (nom_doc,chemin,mail,mail_docteurs,`type`) VALUES ("Radio du crâne","documents/mail@test.com/radio.pdf","mail@test.com","david.legall@gmail.com","Radio");
-INSERT INTO documents (nom_doc,chemin,mail,mail_docteurs,`type`) VALUES ("Comprimés anti-allergie","documents/mail@test.com/ordonnance-alergie.pdf","mail@test.com","arthur.morgann@gmail.com","Ordonnance");
-INSERT INTO documents (nom_doc,chemin,mail,mail_docteurs,`type`) VALUES ("lunettes de vue","documents/mail@test.com/ordonnance-lunettes.pdf","mail@test.com","leon.kennedy@gmail.com","Ordonnance");
-INSERT INTO documents (nom_doc,chemin,mail,mail_docteurs,`type`) VALUES ("Questionnaire avant traitement","documents/mail@test.com/formulaire-cardio.pdf","mail@test.com","john.morston@gmail.com","Formulaire à compléter");
+INSERT INTO documents (nom_doc,chemin,mail,mail_docteurs,`type`) VALUES ("Radio du crâne","documents/mail@test.com/radio.pdf","mail@test.com","david.legall@gmail.com","Radio"),
+("Comprimés anti-allergie","documents/mail@test.com/ordonnance-alergie.pdf","mail@test.com","arthur.morgann@gmail.com","Ordonnance"),
+("lunettes de vue","documents/mail@test.com/ordonnance-lunettes.pdf","mail@test.com","leon.kennedy@gmail.com","Ordonnance"),
+("Questionnaire avant traitement","documents/mail@test.com/formulaire-cardio.pdf","mail@test.com","john.morston@gmail.com","Formulaire à compléter");
+#------------------------------------------------------------
+# Table : facture
+#------------------------------------------------------------
+-- +------------------+--------------+------+-----+---------+
+-- | Field            | Type         | Null | Key | Default |
+-- +------------------+--------------+------+-----+---------+
+-- | identifiant      | int autoicrem| NO   | PRI | NULL    |
+-- | prix_ttc         | float        | NO   |     | NULL    |
+-- | tva              | float        | YES  |     | NULL    |
+-- | date_facturation | datetime     | YES  |     | NULL    |
+-- | date_paiement    | datetime     | YES  |     | NULL    |
+-- | mail             | varchar(255) | NO   | MUL | NULL    |
+-- | mail_docteurs    | varchar(255) | YES  | MUL | NULL    |
+-- | mode_de_paiement | varchar(50)  | NO   | MUL | NULL    |
+-- | nom              | varchar(255) | YES  | MUL | NULL    |
+-- +------------------+--------------+------+-----+---------+
+INSERT INTO facture (prix_ttc,tva,date_facturation,date_paiement,mail,mail_docteurs,mode_de_paiement,nom) VALUES 
+(64.99,10.22,"2022-09-20 10:38:00","2023-10-05 17:05:00","mail@test.com","david.legall@gmail.com","Chèque","CENTRE HOSPITALIER FOUGERES"),
+(42.00,5.21,"2023-02-28 8:28:00","2023-02-28 8:29:00","mail@test.com","arthur.morgann@gmail.com","Carte bancaire","POLYCLINIQUE DU TREGOR"),
+(37.47,27.09,"2020-04-02 13:34:00","2020-04-02 13:34:00","mail@test.com","leon.kennedy@gmail.com","Espèces","CLINIQUE DES GRAINETIERES"),
+(132.47,49.09,"2021-04-09 15:54:00","2021-04-09 17:01:00","mail@test.com","john.morston@gmail.com","Carte bancaire","CLINIQUE DE LA MISERICORDE CAEN");
