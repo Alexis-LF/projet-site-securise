458a2cfc3fa1543203a9d988f219af23a254eba2
Jacques Le Guédès <jacques.le-guedes@isen-ouest.yncrea.fr>
2023-03-02 10:33:37 +0100

    Factures Js

--- FRONT_END/javascript/factures.js
+++ FRONT_END/javascript/factures.js
@@ -37,7 +37,107 @@ function ajaxReponse(type, url, jwt, callback){
     xhr.send(data);
     }
 
-function afficheFactures(liste_factures){
+    function afficheFactures(liste_factures){
+      let html_mere=document.getElementById("liste_facts"); //parent
+      for (i = 0; i < liste_factures.length; i++) {
+    
+        let factures=liste_factures[i];
+    
+        let download=document.createElement("button");
+        let impression=document.createElement("button");
+    
+        let div = document.createElement("div");
+        let identifiant = document.createElement("p");
+        identifiant.appendChild(document.createTextNode('Identifiant : '+ factures["identifiant"]));
+    
+        
+        download.appendChild(document.createTextNode('Télécharger ce document'));
+        download.setAttribute("class", "w3-button w3-block w3-green w3-left-align");
+    
+        impression.appendChild(document.createTextNode('Imprimer ce document'));
+        impression.setAttribute("class", "w3-button w3-block w3-cyan w3-left-align");
+    
+        div.appendChild(download);
+        div.appendChild(impression);
+        div.appendChild(identifiant);
+    
+        let prix_ttc = document.createElement("p");
+        prix_ttc.appendChild(document.createTextNode('Prix TTC : '+ factures["prix_ttc"]));
+        div.appendChild(prix_ttc);
+    
+        let tva = document.createElement("p");
+        tva.appendChild(document.createTextNode('TVA : '+ factures["tva"]));
+        div.appendChild(tva);
+    
+        let date_facturation = document.createElement("p");
+        date_facturation.appendChild(document.createTextNode('Date de facturation : '+ factures["date_facturation"]));
+        div.setAttribute("class", "encadrement");
+        div.appendChild(date_facturation);
+    
+        let date_paiement = document.createElement("p");
+        date_paiement.appendChild(document.createTextNode('Date de paiement : '+ factures["date_paiement"]));
+        div.appendChild(date_paiement);
+    
+        let mode_de_paiement = document.createElement("p");
+        mode_de_paiement.appendChild(document.createTextNode('Mode de paiement : '+ factures["mode_de_paiement"]));
+        div.appendChild(mode_de_paiement);
+    
+        let mail_docteurs = document.createElement("p");
+        mail_docteurs.appendChild(document.createTextNode('Mail du praticien : '+ factures["mail_docteurs"]));
+        div.appendChild(mail_docteurs);
+    
+        let prenom_nom = document.createElement("p"); // enfant
+        prenom_nom.appendChild(document.createTextNode('Nom et prénom du patient : '+ factures["prenom_nom"]));
+        div.appendChild(prenom_nom);
+        div.appendChild(prenom_nom);
+
+        let telephone = document.createElement("p"); // enfant
+        telephone.appendChild(document.createTextNode('Telephone : 0'+ factures["telephone"]));
+        div.appendChild(telephone);
+        div.appendChild(telephone);
+
+        let nom_site = document.createElement("p"); // enfant
+        nom_site.appendChild(document.createTextNode('Nom du site : '+ factures["nom_site"]));
+        div.appendChild(nom_site);
+        div.appendChild(nom_site);
+
+        let adresse = document.createElement("p"); // enfant
+        adresse.appendChild(document.createTextNode('Adresse : '+ factures["adresse"]));
+        div.appendChild(adresse);
+        div.appendChild(adresse);
+
+        let zip_code = document.createElement("p"); // enfant
+        zip_code.appendChild(document.createTextNode('Zip code : '+ factures["zip_code"]));
+        div.appendChild(zip_code);
+        div.appendChild(zip_code);
+
+        let ville = document.createElement("p"); // enfant
+        ville.appendChild(document.createTextNode('Ville : '+ factures["ville"]));
+        div.appendChild(ville);
+        div.appendChild(ville);
+    
+        download.onclick = function cheminAccess() {
+        window.location.href = documents["chemin"];
+        }
+    
+        // Ajouter le div au DOM
+        html_mere.appendChild(div);
+    
+        // Ajouter la fonction d'impression au bouton d'impression
+        impression.onclick = function imprimer_page(){
+          let contenu_facture = div.innerHTML;
+          let page_a_imprimer = '<html><head><title>Facture</title></head><body>' + contenu_facture + '</body></html>';
+          let fenetre_impression = window.open('', '', 'height=500,width=500');
+          fenetre_impression.document.write(page_a_imprimer);
+          fenetre_impression.document.close();
+          fenetre_impression.focus();
+          fenetre_impression.print();
+          fenetre_impression.close();
+        }
+      }
+    }
+
+/* function afficheFactures(liste_factures){
     let html_mere=document.getElementById("liste_facts"); //parent
     for (i = 0; i < liste_factures.length; i++) {
     
@@ -148,7 +248,7 @@ function afficheFactures(liste_factures){
     
   };
   }
-
+*/
   function verificationConnexionReussie(mail, identifiant){
     setCookie("mail", mail);
     setCookie("identifiant", identifiant);
