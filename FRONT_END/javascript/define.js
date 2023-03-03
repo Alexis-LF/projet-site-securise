f9ced06c1910640c68130aa073d65ea7beddd779
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-03-03 12:27:16 +0100

    correction problème mail dans requête

--- FRONT_END/javascript/define.js
+++ FRONT_END/javascript/define.js
@@ -1,11 +1,10 @@
-
 const BASE_URL="http://api.projetm1.fr";
 const API_VERSION="0.07";
 
 const URL_DOCUMENT="index.php/documents?mail=MAIL";
 
-const URL_DOCUMENT_FINAL= BASE_URL+"/"+API_VERSION+"/"+URL_DOCUMENT.replace("MAIL", mail);
+const URL_DOCUMENT_FINAL= BASE_URL+"/"+API_VERSION+"/"+URL_DOCUMENT;
 
 let id=null;
 const URL_FACTURE="index.php/factures?mail=MAIL"// &id=IDENTIFIANT"
-const URL_FACTURE_FINAL=BASE_URL+"/"+API_VERSION+"/"+URL_FACTURE.replace("MAIL", mail)// .replace("IDENTIFIANT", id) 
+const URL_FACTURE_FINAL=BASE_URL+"/"+API_VERSION+"/"+URL_FACTURE;
