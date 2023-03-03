92b0acb8c1456fec0cb7e9ec3025de6d307bb6e9
Jacques Le Guédès <jacques.le-guedes@isen-ouest.yncrea.fr>
2023-03-03 11:13:12 +0100

    define js let inutile

--- FRONT_END/javascript/define.js
+++ FRONT_END/javascript/define.js
@@ -4,7 +4,8 @@ const API_VERSION="0.07";
 
 const URL_DOCUMENT="index.php/documents?mail=MAIL";
 
-const URL_DOCUMENT_FINAL= BASE_URL+"/"+API_VERSION+"/"+URL_DOCUMENT;
+const URL_DOCUMENT_FINAL= BASE_URL+"/"+API_VERSION+"/"+URL_DOCUMENT.replace("MAIL", mail);
+
 let id=null;
 const URL_FACTURE="index.php/factures?mail=MAIL"// &id=IDENTIFIANT"
-const URL_FACTURE_FINAL=BASE_URL+"/"+API_VERSION+"/"+URL_FACTURE// .replace("IDENTIFIANT", id) 
+const URL_FACTURE_FINAL=BASE_URL+"/"+API_VERSION+"/"+URL_FACTURE.replace("MAIL", mail)// .replace("IDENTIFIANT", id) 
