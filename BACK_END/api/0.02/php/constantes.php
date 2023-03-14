5bdd526b9689cc572e86df362cca691adcf4b5af
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-03-14 10:45:03 +0100

    ajout du projet laravel d'exemple

--- BACK_END/api/0.02/php/constantes.php
+++
@@ -1,9 +0,0 @@
-<?php
-// pour le fonctionnement des librairies externes
-require __DIR__ . '/../vendor/autoload.php';
-
-// Récupérer le fichier .env dans le dossier courant
-$dotenv = Dotenv\Dotenv::createImmutable(".");
-$dotenv->load();
-// les variables sont désormais dans  $_ENV[]
-?>
