5dc1db885fb3d378e4b4138642933ed42a513b02
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-03-14 16:35:54 +0100

    Commentaires & fonction débug pour avoir les GET & POST

--- BACK_END/example-docker-app/routes/api.php
+++ BACK_END/example-docker-app/routes/api.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Mode_paiementController;
 use App\Http\Controllers\ProfessionController;
+use App\Http\Controllers\VillesController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,22 +23,33 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Fonction de débuggage À DÉSACTIVER quand inutilisé
+// affiche le contenu des GET et des POST
+Route::get('/debug', function () {
+    return  array("GET"=>$_GET, "POST"=>$_POST);
+});
+
+// un "ping" de l'api
 Route::get('/test', function () {
     return "api !";
 });
 
+// Fonction de mode de paiement 
+// n'est pas utile, pourrait être supprimée
 Route::get('/mode_paiement', function () {
     return Mode_paiementController::mode_de_paiement();
 });
 
+// Fonction de professions 
 Route::get('/professions', function () {
     return ProfessionController::noms();
 });
-
+// Fonction qui retourne les villes 
 Route::get('/villes', function () {
     return VillesController::villes();
 });
 
+// Recherche de docteurs, par rapport au lieu, le nom & la profession
 Route::get('/recherche', function () {
     // return ProfessionController::noms();
 });
