99cb5d1b89cedb48fa78d3246d9177e0e305561e
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-03-15 10:52:33 +0100

    création /factures

---
+++ BACK_END/example-docker-app/app/Http/Controllers/FacturesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+ 
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+
+class FacturesController extends Controller
+{
+    function factures(Request $request){
+
+        
+    }
+
+
+
+}
