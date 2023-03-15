8219754cdd9f78f43c72c36ee40923a6a7eab0fd
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-03-15 10:22:37 +0100

    ville avec where

--- BACK_END/example-docker-app/app/Http/Controllers/VillesController.php
+++ BACK_END/example-docker-app/app/Http/Controllers/VillesController.php
@@ -3,17 +3,22 @@
 namespace App\Http\Controllers;
  
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
  
+define("RECHERCHE_VILLE_MINI", 3);
 class VillesController extends Controller
 {
     /**
-     * Show a list of all of the mode_paiements.
+     * Enumére la liste des villes qui répondent aux conditions  
      */
-    public static function villes()
+    public static function villes(Request $request)
     {
-        $villes = DB::table('villes')->get();
-        return $villes;
+        if ($request->has('name') && (strlen($request->input('name')) >= RECHERCHE_VILLE_MINI)) {
+            $resultat=DB::table('cities')->select('id', 'zip_code', 'name')->where('name', 'like', $request->input('name').'%')->get();
+            return response()->json($resultat);
+        }
+        return response()->json(['error' => 'Conditions non respectée !']);
     }
 }
