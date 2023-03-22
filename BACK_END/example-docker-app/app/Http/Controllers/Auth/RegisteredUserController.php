662918031c7f722facd4ba72203a53a63573a4dc
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-03-22 17:50:25 +0100

    ajout des modifs  de la branche laravel_frontend dans le projet backend actuel

--- BACK_END/example-docker-app/app/Http/Controllers/Auth/RegisteredUserController.php
+++ BACK_END/example-docker-app/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -30,14 +30,22 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        // we check values in form
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        // fill person table
+        DB::table('personne')->insert([
+            'mail' => 'kayla@example.com',
+            'nom' => $request->nom,
+            'telephone' => $request->telephone,
+            'date_naissance' => $request->date_naissance,
+            'prenom' => $request->prenom
+        ]);
+
         $user = User::create([
-            'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
