99b6c341155b20f3a27113b277ef16becc450013
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-03-23 14:51:51 +0100

    connexion & inscription depuis le site laravel

--- FRONT_END/src/javascript/define.js
+++ FRONT_END/src/javascript/define.js
@@ -1,5 +1,9 @@
-const BASE_URL="http://127.0.0.1:80/api";
+const BASE_URL="http://127.0.0.1:8000/api";
 const API_VERSION="2.00";
+
+const AUTH_BASE_URL="http://127.0.0.1:8000"
+const AUTH_LOGIN_URL=AUTH_BASE_URL+"/login"
+const AUTH_REGISTER_URL=AUTH_BASE_URL+"/register"
 
 const URL_DOCUMENT="documents?mail=MAIL";
 
