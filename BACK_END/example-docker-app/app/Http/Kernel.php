c7253d876fa24ff2eaea7c8d16520cc51abe03b4
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-03-24 15:30:18 +0100

    Kernel par défaut

--- BACK_END/example-docker-app/app/Http/Kernel.php
+++ BACK_END/example-docker-app/app/Http/Kernel.php
@@ -36,22 +36,13 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-    ],
-        
+        ],
 
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-        // 'api' => [
-            // 'throttle:api',
-        // ],
     ];
 
     /**
