9295588a17c12b7b60b7e0e9ec6d0caffeaa6791
Alexis LE FLOCH <alexis.le-floch@protonmail.com>
2023-03-29 16:49:58 +0200

    nettoyage des routes

--- BACK_END/example-docker-app/app/Http/Controllers/Controller.php
+++ BACK_END/example-docker-app/app/Http/Controllers/Controller.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Traits\HttpResponses;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests, ValidatesRequests, HttpResponses;
+    public function main()
+    {
+        return $this->success([]);
+    }
+
 }
--- BACK_END/example-docker-app/routes/web.php
+++ BACK_END/example-docker-app/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\UserConnecteController;
+use App\Http\Controllers\Controller;
 
 
 /*
@@ -16,18 +17,4 @@ use App\Http\Controllers\Auth\UserConnecteController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+Route::get('/',[Controller::class,'main']);
